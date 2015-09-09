2c30f66b5101a70468e6420015e5501dfd921831
frankstaas101 <frankstaas101@gmail.com>
2015-09-09 19:18:56 -0400

    psudocode

--- src/main/Validators.java
+++ src/main/Validators.java
@@ -4,15 +4,18 @@ import java.util.ArrayList;
 
 public class Validators {
 	
-	public static void checkRow(ArrayList<Integer> row){
-		
+	public static boolean checkRow(ArrayList<Integer> row){
+		//TODO: Logic code for validation of the row here
+		return true; // if the check is successful
 	}
 	
-	public static void checkColumn(ArrayList<Integer> column){
-		
+	public static boolean checkColumn(ArrayList<Integer> column){
+		//TODO: Logic code for validation of the column here
+		return true; // if the check is successful
 	}
 	
-	public static void checkBox(ArrayList<Integer> row, ArrayList<Integer> col){
-		
+	public static boolean checkBox(ArrayList<Integer> row, ArrayList<Integer> col){
+		//TODO: Logic code for validation of the box here
+		return true; // if the check is successful
 	}
 }
