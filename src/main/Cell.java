536adfc31623bcc86dc30af6ac301d3885923cf6
frankstaas101 <frankstaas101@gmail.com>
2015-09-09 17:56:27 -0400

    Created Project Files
    Created a java project (using Eclipse Mars)

---
+++ src/main/Cell.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Cell {
+	
+	private int row; // the row the number is in
+	private int column; // the column the number is in
+	private int box; // the box the number is in
+	
+	public Cell(int row, int column, int box){
+		this.row = row;
+		this.column = column;
+		this.box = box;
+	}
+}
