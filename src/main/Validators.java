a6ab043a023759c0326f5e2797378f865af2df35
frankstaas101 <frankstaas101@gmail.com>
2015-09-09 19:11:37 -0400

    Added Validators Class

---
+++ src/main/Validators.java
@@ -0,0 +1,18 @@
+package main;
+
+import java.util.ArrayList;
+
+public class Validators {
+	
+	public static void checkRow(ArrayList<Integer> row){
+		
+	}
+	
+	public static void checkColumn(ArrayList<Integer> column){
+		
+	}
+	
+	public static void checkBox(ArrayList<Integer> row, ArrayList<Integer> col){
+		
+	}
+}
