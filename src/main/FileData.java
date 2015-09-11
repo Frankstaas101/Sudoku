608dfb4e9b17238107f8460d330c444080e7c811
Frankstaas101 <frank@Franks-MBP.home>
2015-09-11 09:38:42 -0400

    File data testing
    added file data regex patterns to read in the file and discern between
    a comment and a suduko puzzle pattern.

--- src/main/FileData.java
+++ src/main/FileData.java
@@ -1,9 +1,68 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class FileData {
 
-	public FileData(){
+	int width = 0;
+	int height = 0;
+	int dimension;
+	ArrayList<String> comments = new ArrayList<String>();
 	
+	public FileData() {
+		
+	}
+	
+	public void readFile(File name){
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(name));
+			
+			String nextLine = in.readLine();
+			
+			while(nextLine != null) {
+				//System.out.println(nextLine);
+				
+				// matches a comment format
+				if (nextLine.matches("^c(\\s+).*")){
+					comments.add(nextLine);
+				}
+				// first two numbers are the height and width
+				else if (nextLine.matches("^[1-9]$")){
+					if (width == 0) {
+						width = new Integer(nextLine);
+					}
+					else if (height == 0 && width != 0) {
+						width = new Integer(nextLine);
+					}
+				}
+				// matches the pattern 
+				else if (nextLine.matches("^[1-9\\s+]{" + width * height + "}$")){
+					System.out.println(nextLine);
+				}
+				nextLine = in.readLine();
+			}
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("File was not found!");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("There was a problem with reading the file!");
+			e.printStackTrace();
+		}
+		
 	}
 	
 }
+
+
+// REGEX TESTING
+// Comment regex "^c(\s+).*"
+//c 	test test test 9002423 ,.,.,*(@#*$(#@))
+//333
+
+///4 4 4 4 4 4
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,17 @@
 package main;
 
+import java.io.File;
+
 public class Main {
+	// CHECK FOR MORE THEN THE DIMENSION AMMOUNT OF EACH NUMBER 
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		FileData fd = new FileData();
+		fd.readFile(new File("/Users/frank/Desktop/Repositories/Sudoku/src/main/test.txt"));
 		
+		// Proving comments are being placed into the array
+		for (String s : fd.comments){
+			System.out.println("Comment: " + s);
+		}
 	}
-
 }
