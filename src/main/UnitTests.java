c4e574ec61366379cfc7b80df65dcde936b1695b
SebasWild <wilds62@students.rowan.edu>
2015-09-22 20:30:27 -0400

    Unit Tests
    -Moved BruteSolverTest to UnitTests.java
    -Added some comments to unit tests.
    -Removed BruteSolverTest.java

--- src/main/UnitTests.java
+++ src/main/UnitTests.java
@@ -12,7 +12,9 @@ import org.junit.Test;
 public class UnitTests {
 	
 
-	
+	/*
+	 * Test that rows without duplicates are correctly accepted.
+	 */
 	@Test
     public void testRowCheckingSuccess() {
         
@@ -34,7 +36,9 @@ public class UnitTests {
         assertEquals(true, Functions.checkRow(checkList, dimension, puzzle.cells ));
 
     }
-	
+	/*
+	 * Test that rows with duplicates are correctly rejected.
+	 */
 	@Test
     public void testRowCheckingFailure() {
         
@@ -56,7 +60,9 @@ public class UnitTests {
         assertEquals(false, Functions.checkRow(checkList, dimension, puzzle.cells ));
 
     }
-	
+	/*
+	 * Assert that puzzles with duplicates in the columns are correctly rejected.
+	 */
 	@Test
     public void testColCheckingFailure() {
         
@@ -78,7 +84,9 @@ public class UnitTests {
         assertEquals(false, Functions.checkCol(checkList, dimension, puzzle.cells ));
 
     }
-	
+	/*
+	 * Assert that correct columns are accepted.
+	 */
 	@Test
     public void testColCheckingSuccess() {
         
@@ -100,7 +108,9 @@ public class UnitTests {
         assertEquals(true, Functions.checkCol(checkList, dimension, puzzle.cells ));
 
     }
-	
+	/*
+	 * Assert that boxes with duplicates are rejected properly.
+	 */
 	@Test
     public void testBoxCheckingFailure() {
         
@@ -126,7 +136,9 @@ public class UnitTests {
 
     }
 	
-	
+	/*
+	 * Assert that box checking correctly accepts correct boxes.
+	 */
 	@Test
     public void testBoxCheckingSuccess() {
         
@@ -152,7 +164,9 @@ public class UnitTests {
 
     }
 	
-	
+	/*
+	 * Asser that a correct puzzle passes.
+	 */
 	@Test
 	public void testPuzzleSuccess()
 	{
@@ -175,7 +189,9 @@ public class UnitTests {
         assertEquals(true, Functions.validate(puzzle.cells, puzzle.sections, height, width ));
 	
 	}
-	
+	/*
+	 * Assert that an incorrect puzzle fails.
+	 */
 	@Test
 	public void testPuzzleFailure()
 	{
@@ -192,13 +208,15 @@ public class UnitTests {
 			puzzle = new Puzzle(fd.width, fd.height, fd.puzzle);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			e.printStackTrace(); 
 		}
 
         assertEquals(false, Functions.validate(puzzle.cells, puzzle.sections, height, width ));
 	
 	}
-	
+	/*
+	 * Test that the file reader handles the puzzle, dimension, and comments properly.
+	 */
 	@Test
 	public void testFileReader()
 	{
@@ -215,6 +233,64 @@ public class UnitTests {
 		//height is equal
 		//width is equal
 		
+	}
+	/*
+	 * Assert that BruteSolver actually increments through all numbers
+	 * and handles the end of incrementing properly.
+	 */
+	@Test
+	public void test() {
+		ArrayList<Cell> testCells = new ArrayList<Cell>();		
+		testCells.add(new Cell(0, 0));		//Make some test cells.
+		testCells.add(new Cell(0, 0));
+		testCells.add(new Cell(0, 0));
+		
+		testCells = BruteSolver.initializeValues(testCells);		//Set all the cell values to 1
+		for(Cell c: testCells)
+		{
+			assertTrue(c.value == 1);		//Test if the values were set correctly.
+		}
+		
+			//Test if the incrementing works as expected, with an upper bound of 2 for the values.
+			
+			testCells = BruteSolver.assignValues(testCells, 2);	//The first increment call
+			assertTrue(testCells.get(0).value == 2);
+			assertTrue(testCells.get(1).value == 1);
+			assertTrue(testCells.get(2).value == 1);
+			testCells = BruteSolver.assignValues(testCells, 2);
+			assertTrue(testCells.get(0).value == 1);
+			assertTrue(testCells.get(1).value == 2);
+			assertTrue(testCells.get(2).value == 1);
+			testCells = BruteSolver.assignValues(testCells, 2);
+			assertTrue(testCells.get(0).value == 2);
+			assertTrue(testCells.get(1).value == 2);
+			assertTrue(testCells.get(2).value == 1);
+			testCells = BruteSolver.assignValues(testCells, 2);
+			assertTrue(testCells.get(0).value == 1);
+			assertTrue(testCells.get(1).value == 1);
+			assertTrue(testCells.get(2).value == 2);
+			testCells = BruteSolver.assignValues(testCells, 2);
+			assertTrue(testCells.get(0).value == 2);
+			assertTrue(testCells.get(1).value == 1);
+			assertTrue(testCells.get(2).value == 2);
+			testCells = BruteSolver.assignValues(testCells, 2);
+			assertTrue(testCells.get(0).value == 1);
+			assertTrue(testCells.get(1).value == 2);
+			assertTrue(testCells.get(2).value == 2);
+			testCells = BruteSolver.assignValues(testCells, 2);
+			assertTrue(testCells.get(0).value == 2);
+			assertTrue(testCells.get(1).value == 2);
+			assertTrue(testCells.get(2).value == 2);
+			
+			//Try to increment one more time. Since we have exhausted all combinations this should return null.
+			assertTrue((testCells = BruteSolver.assignValues(testCells, 2)) == null);
+			
+			//Edge case: try to increment a number which cannot be incremented any further.
+			testCells = new ArrayList<Cell>();	
+			testCells.add(new Cell(1, 0));		//test cell has a value of 1
+			//Since the cell's value is already at its maximum, trying to increment will return null
+			assertTrue((testCells = BruteSolver.assignValues(testCells, 1)) == null);		
+	
 	}
 
 	
