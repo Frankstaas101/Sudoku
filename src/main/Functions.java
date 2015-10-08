189f5ba12bc5a7e57b188309a4c56d7439a75546
frankstaas101 <frankstaas101@gmail.com>
2015-10-07 21:24:58 -0400

    Made the hasDuplicates function more effiecent.
    Instead of using a hashmap, I dumped all the values in the list into a
    hashset via the hashset constructor. After the values were placed into
    the hashset I then compared the sizes of the hashset and the original
    list. If the hash set is smaller then the list then a duplicate exists
    in the list. The boolean result is then returned.

--- src/main/Functions.java
+++ src/main/Functions.java
@@ -2,28 +2,35 @@ package main;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /*
  * NOT NEEDED?
  */
 public class Functions {
 
-	// Returns true if there is a duplicate
-	public static boolean hasDuplicate(ArrayList<Integer> array){
+	/**
+	 * Returns whether or not there is a duplicate in the list
+	 * @param list to be checked for duplicates
+	 * @return true if a duplicate exists otherwise false
+	 */
+	public static boolean hasDuplicate(List<Integer> list){
 
-		int size = array.size(); // size of array
+		// boolean to be returned
 		boolean hasDuplicate = false; 
-		HashMap<Integer, Integer> checkMap = new HashMap<Integer, Integer>();
 
-		for(Integer i: array) {
-			checkMap.put(i, 0);
-		}
+		// store all values of the array in the hash set
+		Set<Integer> checkingSet = new HashSet<Integer>(list);
 
-		if (checkMap.size() != size) {
+		// if the set contains less values then the array then
+		// there is a duplicate value in the list
+		if (checkingSet.size() != list.size()) {
 			hasDuplicate = true;
 		}
 
-		return hasDuplicate ? true : false;
+		return hasDuplicate;
 	}
 
 
@@ -38,107 +45,69 @@ public class Functions {
 		if(checkRow(checkList, dimension, puzzle) &&
 				checkCol(checkList, dimension, puzzle) &&
 				checkBoxes(checkList, dimension, puzzle, sections))
-					return true;
+			return true;
 		return false;
 	}
 
 
-public static boolean checkRow(ArrayList<Integer> checkList, int dimension, ArrayList<Cell> puzzle)
-{
-	// Rows - starts at 1 counts to dimension breaks the arrays by rows
-			for(int i = 0; i < puzzle.size(); i++) { // iterates though ever value in the puzzle
-				checkList.add(puzzle.get(i).value);
-				if ((i+1)  % dimension == 0) { // this check happens every "dimension" values
-					if (hasDuplicate(checkList)) {
-						//valid = false; 
-						return false;		// break out as soon as a false value is found
-					}
-					checkList.clear(); // clears the list if not false to be used again
-				}
-			}
-			return true;
-	
-}
-	
-		
-		public static boolean checkCol(ArrayList<Integer> checkList, int dimension, ArrayList<Cell> puzzle)
-		{
-			// Column
-			for (int colNum = 0; colNum < dimension; colNum++) { // does this dimension times
-				for(int i = 0; i < puzzle.size(); i++) { // iterate through whole puzzle
-					if ((i + colNum)  % dimension == 0) { // next row
-						checkList.add(puzzle.get(i).value);	// adds the first value of the next row
-					}
-				}
+	public static boolean checkRow(ArrayList<Integer> checkList, int dimension, ArrayList<Cell> puzzle)
+	{
+		// Rows - starts at 1 counts to dimension breaks the arrays by rows
+		for(int i = 0; i < puzzle.size(); i++) { // iterates though ever value in the puzzle
+			checkList.add(puzzle.get(i).value);
+			if ((i+1)  % dimension == 0) { // this check happens every "dimension" values
 				if (hasDuplicate(checkList)) {
-					//valid =  false; 
+					//valid = false; 
 					return false;		// break out as soon as a false value is found
 				}
-				checkList.clear(); // clears the list
+				checkList.clear(); // clears the list if not false to be used again
 			}
-			return true;
 		}
-		
-		
-//		 0  1  2  3
-//		 4  5  6  7
-//		 8  9  10 11
-//		 12 13 14 15
-		//Sections
-		
-		public static boolean checkBoxes(ArrayList<Integer> checkList, int dimension, ArrayList<Cell> puzzle, ArrayList<ArrayList<Integer>> sections)
-		{
-//			 0  1  2  3
-//			 4  5  6  7
-//			 8  9  10 11
-//			 12 13 14 15
-			//Sections
-			
-			//Check all the sections		
-			for(ArrayList<Integer> section: sections)
-			{
-				ArrayList<Integer> sectionValues = new ArrayList<>();		//Create a new List of values for each section.
-				for(Integer i: section)		//Iterate through all the indexes in each section.
-				{
-					sectionValues.add(puzzle.get(i).value);		//Get the value at the given index and add it to the section.
-				}
-				if(hasDuplicate(sectionValues))		//Test the section values.
-				{
-					//valid = false;
-					return false;
-				}
-			}
-			return true;
-		}
-}
-		
-		
-		
-		
-		/* LEGACY CODE BELOW!
-		int location = 0;
-		for(int i = 0; i < dimension; i++){
-		
-			System.out.println("Section: " + i);
-			 
-				//location = i * width * ((dimension * height) * w1);
-			
-			
-			for (int h = 0; h < height; h++){
-				for (int w = 0; w < width; w++){
-					System.out.println(location + ", ");
-					checkList.add(puzzle.get(location));
-					location++;
-				}
-				for (int h1 = 0; h1 < height - 1; h1++){
-					location += width;
+		return true;
+
+	}
+
+
+	public static boolean checkCol(ArrayList<Integer> checkList, int dimension, ArrayList<Cell> puzzle)
+	{
+		// Column
+		for (int colNum = 0; colNum < dimension; colNum++) { // does this dimension times
+			for(int i = 0; i < puzzle.size(); i++) { // iterate through whole puzzle
+				if ((i + colNum)  % dimension == 0) { // next row
+					checkList.add(puzzle.get(i).value);	// adds the first value of the next row
 				}
 			}
 			if (hasDuplicate(checkList)) {
-				valid = false; // break out as soon as a false value is found
+				//valid =  false; 
+				return false;		// break out as soon as a false value is found
 			}
-			
 			checkList.clear(); // clears the list
 		}
+		return true;
+	}
 
-		*/
+	// Sections Example:
+	//		 0  1  2  3
+	//		 4  5  6  7
+	//		 8  9  10 11
+	//		 12 13 14 15
+	// Section 0 = 0 1 4 5
+	public static boolean checkBoxes(ArrayList<Integer> checkList, int dimension, ArrayList<Cell> puzzle, ArrayList<ArrayList<Integer>> sections)
+	{
+		//Check all the sections		
+		for(ArrayList<Integer> section: sections)
+		{
+			ArrayList<Integer> sectionValues = new ArrayList<>();		//Create a new List of values for each section.
+			for(Integer i: section)		//Iterate through all the indexes in each section.
+			{
+				sectionValues.add(puzzle.get(i).value);		//Get the value at the given index and add it to the section.
+			}
+			if(hasDuplicate(sectionValues))		//Test the section values.
+			{
+				//valid = false;
+				return false;
+			}
+		}
+		return true;
+	}
+}
